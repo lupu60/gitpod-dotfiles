68f86a57cf02c32029ec53e6b344e00ba148cc91
bogdanlupu <lupu60@gmail.com>
2024-01-29 11:39:01 +0200

    Add zsh plugins to install script

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 sudo apt-get install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 
 # Install packages
