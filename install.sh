ec2fde6ef808d7e49b28de400c88be3ed9c68671
lupu <lupu60@gmail.com>
2025-01-28 13:41:29 +0200

    Add Television CLI tool installation and refactor Lazygit and Lazydocker setup

--- install.sh
+++ install.sh
@@ -7,29 +7,20 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config
 curl https://raw.githubusercontent.com/lupu60/gitpod-dotfiles/main/starship.toml > ~/.config/starship.toml
 
+# Install packages
+sudo apt-get update
+sudo apt-get install htop fzf ripgrep bat --yes
+sudo ln -s /usr/bin/batcat /usr/bin/bat
+# sudo apt install universal-ctags
+
+# Lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
-# Install packages
-sudo apt-get update
-sudo apt-get install htop fzf ripgrep bat --yes
-sudo ln -s /usr/bin/batcat /usr/bin/bat
-sudo apt install universal-ctags
-
-
 # Lazydocker
-# curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-
-# # Nvim
-# wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-# tar xzvf nvim-linux64.tar.gz
-# chmod +x ./nvim-linux64/bin/nvim
-# sudo ln -s "$(realpath ./nvim-linux64/bin/nvim)" /usr/bin/nvim
-
-# NvChad
-# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim -Es
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
 # Git SPR
 # echo "deb [trusted=yes] https://apt.fury.io/inigolabs/ /" | sudo tee /etc/apt/sources.list.d/inigolabs.list
@@ -52,6 +43,14 @@ source <(av completion bash)
 # sudo apt-get install /workspace/git-town.deb --yes
 
 
+# Wakatime
 if test -v WAKATIME_API; then {
     printf '%s\n' '[settings]' "api_key = ${WAKATIME_API}"  "hide_file_names = true"  "hide_project_names = false"  "hide_dependencies = true"  "hide_branch_names = true"  "hide_project_folder = true"  "hostname = workhorse" "debug = false" > "$HOME/.wakatime.cfg";
 } fi
+
+
+# Television
+VER=`curl -s "https://api.github.com/repos/alexpasmantier/television/releases/latest" | grep '"tag_name":' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/'`
+curl -LO https://github.com/alexpasmantier/television/releases/download/$VER/television_$VER-1_amd64.deb
+echo $VER
+sudo dpkg -i television_$VER-1_amd64.deb
