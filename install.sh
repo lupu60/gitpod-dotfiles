330b260370763657c85dd0f31b710c7f341c1686
lupu <5337610+lupu60@users.noreply.github.com>
2025-02-14 15:55:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ curl https://raw.githubusercontent.com/lupu60/gitpod-dotfiles/main/starship.toml
 
 # Install packages
 sudo apt-get update
-sudo apt-get install htop fzf ripgrep bat --yes
+sudo apt-get install htop fzf ripgrep bat fd-find --yes
 sudo ln -s /usr/bin/batcat /usr/bin/bat
+sudo ln -s $(which fdfind) /bin/fd
 
 # sudo apt install universal-ctags
 
