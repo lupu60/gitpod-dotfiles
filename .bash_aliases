d39948f9f08e52ae8fae3c3d84d22cb44e2fe159
lupu <5337610+lupu60@users.noreply.github.com>
2025-10-24 14:36:30 +0300

    Add installation of bash aliases and git configuration to install.sh

--- .bash_aliases
+++ .bash_aliases
@@ -1,23 +1,4 @@
 # Aliases
 alias refresh_backend="cd /workspace/shares/projects/backend; git pull origin dev --ff-only && pnpm i && pnpm db:update"
 alias ll="ls -alF"
-
-git config --global fetch.parallel 5
-git config --global fetch.pruneTags true
-git config --global core.compression 9
-
-git config --global alias.pull-depth '!f() { git pull --depth=1 --no-recurse-submodules; }; f'
-git config --global alias.cmpn '!f() { git commit -m "$@" --no-verify && git push --no-verify; }; f'
-git config --global alias.cmp '!f() { git commit -m "$@" && git push; }; f'
-git config --global alias.ac '!f() { git add -A && git commit -m "$@"; }; f'
-
-git config --global alias.s status
-git config --global alias.c commit  
-git config --global alias.co checkout
-git config --global alias.p push  
-git config --global alias.pl pull
-
 alias githist='git log --abbrev-commit --oneline $(git merge-base origin/dev HEAD)^..HEAD'
-
-# mkdir $HOME/.local/bin
-# echo '#!/bin/bash' > ~/.local/bin/code && echo "exec $(which cursor) \"\$@\"" >> ~/.local/bin/code && chmod +x ~/.local/bin/code
--- install.sh
+++ install.sh
@@ -7,6 +7,12 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config
 curl https://raw.githubusercontent.com/lupu60/gitpod-dotfiles/main/starship.toml > ~/.config/starship.toml
 
+# Install bash aliases
+curl https://raw.githubusercontent.com/lupu60/gitpod-dotfiles/main/.bash_aliases > ~/.bash_aliases
+if ! grep -q "source ~/.bash_aliases" ~/.bashrc; then
+    echo 'source ~/.bash_aliases' >> ~/.bashrc
+fi
+
 # Install packages
 sudo apt-get update
 sudo apt-get install htop fzf ripgrep bat fd-find --yes
@@ -17,3 +23,21 @@ sudo npm install -g @anthropic-ai/claude-code -Y
 
 # Television
 curl -fsSL https://alexpasmantier.github.io/television/install.sh | bash
+
+
+
+## git config
+git config --global fetch.parallel 5
+git config --global fetch.pruneTags true
+git config --global core.compression 9
+
+git config --global alias.pull-depth '!f() { git pull --depth=1 --no-recurse-submodules; }; f'
+git config --global alias.cmpn '!f() { git commit -m "$@" --no-verify && git push --no-verify; }; f'
+git config --global alias.cmp '!f() { git commit -m "$@" && git push; }; f'
+git config --global alias.ac '!f() { git add -A && git commit -m "$@"; }; f'
+
+git config --global alias.s status
+git config --global alias.c commit
+git config --global alias.co checkout
+git config --global alias.p push
+git config --global alias.pl pull
